0aad59ce3043bb6e1cee647e570bfcc07a00ed8e
Hoshiyom1 <108411178+Hoshiyom1@users.noreply.github.com>
2023-11-20 23:21:31 +0800

    Update TOKEN.sol

--- TOKEN.sol
+++ TOKEN.sol
@@ -6,20 +6,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract MyERC20Token is ERC20, Ownable, ERC20Burnable {
-    constructor() ERC20("Sally", "Hoshiom1") Ownable(msg.sender) {} 
+    constructor() ERC20("Sally", "Hoshiyom1") Ownable(msg.sender) {} 
     
   // Mint
-   function mintTokens(address to, uint256 amount) external onlyOwner {
+   function MINT(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 
   // Burn 
-  function burnTokens(uint256 amount) external {
+  function BURN(uint256 amount) external {
         _burn(msg.sender, amount);
     }
 
     //Transfer 
-    function transferTokens(address receiver, uint256 amount) external {
+    function TRANSFER(address receiver, uint256 amount) external {
         _transfer(msg.sender, receiver, amount);
     }
 }
